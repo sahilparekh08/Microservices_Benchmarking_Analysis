62fc3ab1dcce41e808ab38c78dc99aa5935ba37b
Sahil Parekh <sahilparekh08@gmail.com>
2025-03-07 16:10:16 -0500

    trying to change perf output ts

--- scripts/collect_perf_data.sh
+++ scripts/collect_perf_data.sh
@@ -63,6 +63,7 @@ echo "sudo perf script -i \"${CONTAINER_NAME}.data\" --ns > perf_output.txt || e
 sudo perf script -i "${CONTAINER_NAME}.data" --ns > perf_output.txt || exit 1
 
 PERF_DATA_CSV_PATH="$DATA_DIR/data/perf_data.csv"
+PERF_UPDATED_TS_CSV_PATH="$DATA_DIR/data/perf_updated_ts.csv"
 
 echo "echo \"Time,Frequency,Type\" > $PERF_DATA_CSV_PATH"
 echo "Time,Frequency,Type" > $PERF_DATA_CSV_PATH
@@ -73,6 +74,19 @@ echo "awk '/LLC-loads/ {gsub(\":\", \"\", \$3); print \$3 \",\" \$4 \",LOAD\"}
 awk '/LLC-loads/ {gsub(":", "", $3); print $3 "," $4 ",LOAD"} 
      /LLC-load-misses/ {gsub(":", "", $3); print $3 "," $4 ",MISS"}
      /instructions/ {gsub(":", "", $3); print $3 "," $4 ",INSTRUCTIONS"}' perf_output.txt >> "$PERF_DATA_CSV_PATH" || exit 1
+
+echo "awk -v boot_time=$BOOT_TIME 'BEGIN {FS=","; OFS=","} NR > 1 { \\
+    split(\$1, time_parts, \".\"); \\
+    timestamp_ns = (boot_time + time_parts[1]) * 1000000000 + time_parts[2]; \\
+    \$1 = timestamp_ns; \\
+    print \$1, \$2, \$3 \\
+}' \"$PERF_DATA_CSV_PATH\" > \"$PERF_UPDATED_TS_CSV_PATH\""
+awk -v boot_time=$BOOT_TIME 'BEGIN {FS=","; OFS=","} NR > 1 { \
+    split($1, time_parts, "."); \
+    timestamp_ns = (boot_time + time_parts[1]) * 1000000000 + time_parts[2]; \
+    $1 = timestamp_ns; \
+    print $1, $2, $3 \
+}' "$PERF_DATA_CSV_PATH" > "$PERF_UPDATED_TS_CSV_PATH"
 
 echo "sudo rm -f \"${CONTAINER_NAME}.data\""
 sudo rm -f "${CONTAINER_NAME}.data"
